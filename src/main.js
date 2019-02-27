b1b66cc8c4788865ae84e4a734965e09f0278329
KarenAmicone <karen_amicone@hotmail.com>
2019-02-27 16:37:30 -0600

    Arrays en main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,4 @@
+const romantic = ['The lunchbox', 'Up in the air', 'Carol', 'The big sick', 'Casablanca', 'Her', 'Shakespeare in Love','About a boy', 'Before sunrise', 'The Hustler', 'The African Queen','The Artist', 'Your Name', 'Enough Said', 'Midnight in Paris', 'The Graduate', 'The Duke of Burgundy', 'Crazy Heart', 'Dave', 'Breathe In']
+const funny=['Lady Bird','La la land','The Death of Stalin','Crazy Rich Asians','Sorry to Bother You','Bullets Over Broadway','','','','','','','','','', '', '', '', '', '']
+
 
