83cc9f2f5e15aac14e94d428ebcd6ab5956c5711
DianaLiz11 <dianamunoz86@live.com.mx>
2019-02-28 15:02:26 -0600

    funcion para obtener mas de una pelicula

--- src/main.js
+++ src/main.js
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-const movieArrays = [
-  ['The lunchbox', 'Up in the air', 'Carol', 'The big sick', 'Casablanca', 'Her', 'Shakespeare in Love', 'About a boy', 'Before sunrise', 'The Hustler', 'The African Queen', 'The Artist', 'Your Name', 'Enough Said', 'Midnight in Paris', 'The Graduate', 'The Duke of Burgundy', 'Crazy Heart', 'Dave', 'Breathe In'],
-  ['Lady Bird', 'La la land', 'The Death of Stalin', 'Crazy Rich Asians', 'Sorry to Bother You', 'Bullets Over Broadway', 'Blindspotting', 'Old School', 'O Brother', 'The Heat', 'Game Night', 'Pulp Fiction', 'The Hangover', 'Dumb and Dumber', 'School of Rock', 'Hot Fuzz', 'The Breakfast Club', 'Burn After Reading', 'Bachelorette', 'The Birdcage'],
-  ['A Quiet Place', 'It Follows', 'Let the Right One In', 'The Cabin in the Woods', 'A Girl Walks Home Alone at Night', 'The Blair Witch Project', 'The Texas Chainsaw Massacre', 'Night of the Living Dead', 'Bone Tomahawk', 'The Shining', 'Get Out', 'Audition', 'The Thing', 'Evil Dead', 'Funny Games', 'Mulholland Dr.', 'Martyrs', 'The Orphanage', 'Suspiria', 'A Nightmare on Elm Street'],
-  ['The Prestige', 'The Game', 'The Kindergarten Teacher', 'The Silence of the Lambs', 'Twelve Monkeys', 'Black Swan', 'Oldboy', 'Primal Fear', 'Red Dragon', 'The Bone Collector', 'Border', 'Overlord', 'The House That Jack Built', 'Missing', 'Side Effects', 'Let Me In', 'The Imitation Game', 'Gerald’s Game', 'The Gift', 'Raw'],
-  ['The Wizard of Oz', 'Inside Out', 'E.T. The Extra-Terrestrial', 'Coco', 'Paddington 2', 'Incredibles 2', 'Snow White and the Seven Dwarfs', 'Toy Story 2', 'Up', 'Toy Story 3', 'Finding Nemo', 'Pinocchio', 'Toy Story', 'Mary Poppins', 'How to Train Your Dragon', 'The Incredibles', 'My Life as a Zucchini', 'Song Of The Sea', 'About Elly', 'Spirited Away', '101 Dalmatians'],
-  ['The Cabinet of Dr. Caligari', 'Metropolis', 'The Battle of Algiers', 'M', 'Seven Samurai', 'The 400 Blows', 'The Wages of Fear', 'Open City', 'Tokyo Story', 'The Leopard', 'Battleship Potemkin', 'Au Hasard Balthazar', 'The Conformist', 'Let the Right One In', 'Playtime', 'Faces Places', 'Things to Come', 'Only Yesterday', 'Tampopo', 'Three Colors: Red'],
-  ['Black Panther', 'Mad Max: Fury Road', 'King Kong', 'The Adventures of Robin Hood', 'Spider-Man: Homecoming', 'Seven Samurai', 'The Treasure of the Sierra Madre', 'Lawrence of Arabia', 'Captain America: Civil War', 'The Hurt Locker', 'Aliens', 'The Searchers', 'The Terminator', 'The African Queen', 'Once Upon a Time in the West', 'Game of Thrones', 'The Last Kingdom', 'Casino Royale', 'Crouching Tiger, Hidden Dragon', 'Aquaman']
-];
-
-const moods = document.getElementsByClassName("mood");
-
-const obtainDataJson = (title) => {
-  fetch('http://www.omdbapi.com/?apikey=2227c3b4&t=' + title + '&type=movie&plot=full')
-    .then(response => response.json())
-    .then(dataMovies => {
-      console.log(dataMovies);
-    });
-};
-=======
 const movieArrays= [
 ['The lunchbox', 'Up in the air', 'Carol', 'The big sick', 'Casablanca', 'Her', 'Shakespeare in Love', 'About a boy', 'Before sunrise', 'The Hustler', 'The African Queen', 'The Artist', 'Your Name', 'Enough Said', 'Midnight in Paris', 'The Graduate', 'The Duke of Burgundy', 'Crazy Heart', 'Dave', 'Breathe In'],
 ['Lady Bird', 'La la land', 'The Death of Stalin', 'Crazy Rich Asians', 'Sorry to Bother You', 'Bullets Over Broadway', 'Blindspotting', 'Old School', 'O Brother', 'The Heat', 'Game Night', 'Pulp Fiction', 'The Hangover', 'Dumb and Dumber', 'School of Rock', 'Hot Fuzz', 'The Breakfast Club', 'Burn After Reading', 'Bachelorette', 'The Birdcage'],
@@ -35,11 +14,13 @@ let dataMoviesProperties={};
 for (let i=0; i<moods.length; i++){
     moods[i].addEventListener("click", ()=> {
         const moodValue= parseInt(moods[i].value);
-        const arraySelected=movieArrays[moodValue];   
-        const movieTitle= window.data.randomMovies(arraySelected);    
-        obtainDataJson(movieTitle);    
-    })   
-} 
+        const arraySelected=movieArrays[moodValue];
+        // const movieTitle= window.data.randomMovies(arraySelected);
+        // obtainDataJson(movieTitle);
+        selectedMovies = obtainMovies(arraySelected);
+        console.log(selectedMovies);
+    })
+}
 
 
 const obtainDataJson = (title) => {
@@ -56,27 +37,15 @@ const obtainDataJson = (title) => {
             };
             console.log(dataMoviesProperties);
             })
-    
+
         }
->>>>>>> upstream/master
 
 const obtainMovies = (movieArraySelected) => {
   let selectedMovies = [];
-  for(let i = 0; i < 3; i++){
+  for(let i = 0; i < 2; i++){
     const movieTitle = window.data.randomMovies(movieArraySelected);
-    obtainDataJson(movieTitle);
-    selectedMovies.push(movie);
+    obtainDataJson(movieTitle)
+    selectedMovies.push(dataMoviesProperties);
   }
   return selectedMovies;
-}
-
-for (let i = 0; i < moods.length; i++) {
-  moods[i].addEventListener("click", () => {
-    const moodValue = parseInt(moods[i].value);
-    const arraySelected = movieArrays[moodValue];
-    // const movieTitle = window.data.randomMovies(arraySelected);
-    // obtainDataJson(movieTitle);
-    let selectedMovies = obtainMovies(arraySelected);
-    console.log(selectedMovies);
-  })
 }
